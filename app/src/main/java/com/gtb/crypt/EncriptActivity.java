6ce3ddd24a68948db38d337083ae992d7fd7c7c1
GroverTB <grover.jtb@gmail.com>
2016-06-10 14:13:13 -0500

    [ADD] view CRIPT

--- app/src/main/java/com/gtb/crypt/EncriptActivity.java
+++ app/src/main/java/com/gtb/crypt/EncriptActivity.java
@@ -3,15 +3,51 @@ package com.gtb.crypt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.scottyab.aescrypt.AESCrypt;
+
+import java.security.GeneralSecurityException;
 
 public class EncriptActivity extends AppCompatActivity {
-
+    EditText txtContent,txtresult;
+    Button btncript,btndecript;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encript);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        txtContent = (EditText) findViewById(R.id.txtcontent);
+        txtresult = (EditText) findViewById(R.id.txtresult);
+        btncript   = (Button) findViewById(R.id.btncript);
+        btndecript = (Button) findViewById(R.id.btndecript);
+
+        btndecript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String decryptmsj = AESCrypt.decrypt ("droidplayer", txtContent.getText().toString());
+                    txtresult.setText(decryptmsj);
+                }catch (GeneralSecurityException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        btncript.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String encryptmsj = AESCrypt.encrypt("droidplayer", txtContent.getText().toString());
+                    txtresult.setText(encryptmsj);
+                }catch (GeneralSecurityException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
